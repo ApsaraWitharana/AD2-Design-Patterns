e72c1d3e2517d0dcc6d534abcad3a4f22b06127e
ApsaraWitharana <asachini095@gmail.com>
2025-02-21 18:09:45 +0530

    add creational patterns singleton design

---
+++ Creational patterns/Singleton Design Pattern/src/GlobalHandler.java
@@ -0,0 +1,7 @@
+
+
+public class GlobalHandler {
+    public GlobalHandler() {
+        System.out.println("Global Handler instance created...!");
+    }
+}
---
+++ Creational patterns/Singleton Design Pattern/src/Main.java
@@ -0,0 +1,17 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        GlobalHandler globalHandler1 = Singleton.getInstance().getGlobalHandler();
+        GlobalHandler globalHandler2 = Singleton.getInstance().getGlobalHandler();
+        GlobalHandler globalHandler3 = Singleton.getInstance().getGlobalHandler();
+
+        System.out.println(globalHandler1.equals(globalHandler2));
+        System.out.println(globalHandler2.equals(globalHandler3));
+        System.out.println(globalHandler1.equals(globalHandler3));
+
+        System.out.println(globalHandler1.hashCode());
+        System.out.println(globalHandler2.hashCode());
+        System.out.println(globalHandler3.hashCode());
+    }
+}
